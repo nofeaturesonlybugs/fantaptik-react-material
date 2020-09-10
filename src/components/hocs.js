6f8cab9ac17281ea3549315365a2aff16400c958
Nofeatures Onlybugs <nofeaturesonlybugs@gmail.com>
2020-09-10 13:35:30 -0700

    src/components/hocs.js: Comment out unused/uneeded hocs functions.

--- src/components/hocs.js
+++ src/components/hocs.js
@@ -1,35 +1,35 @@
 import React from 'react';
-import M from 'materialize-css';
+// import M from 'materialize-css'; // TODO RM
 import hoistStatics from 'hoist-non-react-statics';
 
-import { merge } from './common';
+// import { merge } from './common'; // TODO RM
 
 const hocs = {
-    // TODO RM
+    // TODO RM 0.0.1
     // Carousel wraps the given component and enables usage of a `carousel` property; when set to
     // `true` the `carousel` property allows the component to be used within a `Carousel`.
-    Carousel : Component => props => {
-        let { carousel, className, ...rest } = props;
-        let addClasses = [];
-        carousel === true && addClasses.push( "" );
-        className = merge`${className} ${addClasses}`
-        return (
-            <Component {...rest} className={className} />
-        );
-    },
+    // Carousel : Component => props => {
+    //     let { carousel, className, ...rest } = props;
+    //     let addClasses = [];
+    //     carousel === true && addClasses.push( "" );
+    //     className = merge`${className} ${addClasses}`
+    //     return (
+    //         <Component {...rest} className={className} />
+    //     );
+    // },
 
-    // TODO RM
+    // TODO RM 0.0.1
     // Carousel wraps the given component and enables usage of a `carousel` property; when set to
     // `true` the `carousel` property allows the component to be used within a `Carousel`.
-    Carousel_OLD : Component => props => {
-        let { carousel, className, ...rest } = props;
-        let addClasses = [];
-        carousel === true && addClasses.push( "carousel-item" );
-        className = merge`${className} ${addClasses}`
-        return (
-            <Component {...rest} className={className} />
-        );
-    },
+    // Carousel_OLD : Component => props => {
+    //     let { carousel, className, ...rest } = props;
+    //     let addClasses = [];
+    //     carousel === true && addClasses.push( "carousel-item" );
+    //     className = merge`${className} ${addClasses}`
+    //     return (
+    //         <Component {...rest} className={className} />
+    //     );
+    // },
 
     // TODO RM
     // Grid wraps the given component and enables usage of a `grid` property; used as a boolean
@@ -89,61 +89,63 @@ const hocs = {
         return hoistStatics( Component, Source );
     },
 
+    // TODO RM 0.0.1
     // List wraps the given component and enables usage of a `list` property; when set to `true`
     // the `list` property allows the component to be used within a `List`.
-    List : Component => props => {
-        let { list, className, ...rest } = props;
-        let addClasses = [];
-        list === true && addClasses.push( 'collection-item' );
-        className = merge`${className} ${addClasses}`;
-        return (
-            <Component {...rest} className={className} />
-        );
-    },
+    // List : Component => props => {
+    //     let { list, className, ...rest } = props;
+    //     let addClasses = [];
+    //     list === true && addClasses.push( 'collection-item' );
+    //     className = merge`${className} ${addClasses}`;
+    //     return (
+    //         <Component {...rest} className={className} />
+    //     );
+    // },
 
+    // TODO RM 0.0.1
     // Modal allows the componet to be used as a modal.  When `modal` is `true` the component is
     // rendered as a modal.  To close the modal the component should be unmounted.
-    Modal_OLD : Component => class extends React.Component {
-        constructor( props ) {
-            super( props );
-            this.ref = React.createRef();
-        }
+    // Modal_OLD : Component => class extends React.Component {
+    //     constructor( props ) {
+    //         super( props );
+    //         this.ref = React.createRef();
+    //     }
 
-        componentDidMount() {
-            const { dismissible, modal } = this.props;
-            if( this.ref && this.ref.current && modal ) {
-                console.log("hocs.Modal.didMount",this.ref,this.ref.current);//TODO RM
-                const elems = M.Modal.init( this.ref.current, { dismissible, onCloseEnd : this.close }  );
-                elems.open();//TODO
-            }
-        }
+    //     componentDidMount() {
+    //         const { dismissible, modal } = this.props;
+    //         if( this.ref && this.ref.current && modal ) {
+    //             console.log("hocs.Modal.didMount",this.ref,this.ref.current);//TODO RM
+    //             const elems = M.Modal.init( this.ref.current, { dismissible, onCloseEnd : this.close }  );
+    //             elems.open();//TODO
+    //         }
+    //     }
 
-        componentWillUnmount() {
-            const { modal } = this.props;
-            if( this.ref && this.ref.current && modal ) {
-                M.Modal.getInstance( this.ref.current ).close();
-                M.Modal.getInstance( this.ref.current ).destroy();
-            }
-        }
+    //     componentWillUnmount() {
+    //         const { modal } = this.props;
+    //         if( this.ref && this.ref.current && modal ) {
+    //             M.Modal.getInstance( this.ref.current ).close();
+    //             M.Modal.getInstance( this.ref.current ).destroy();
+    //         }
+    //     }
     
-        // close is passed when creating the modal and passes the close event to the owner
-        // of the modal.
-        close = () => {
-            const { onDismissed } = this.props;
-            onDismissed && onDismissed();
-        }
+    //     // close is passed when creating the modal and passes the close event to the owner
+    //     // of the modal.
+    //     close = () => {
+    //         const { onDismissed } = this.props;
+    //         onDismissed && onDismissed();
+    //     }
     
-        render() {
-            let { modal, className, ...rest } = this.props;
-            return modal ? (
-                <div ref={this.ref} className={merge`${className} modal`}>
-                    <Component {...rest}/>
-                </div>
-            ) : (
-                <Component className={merge`${className}`} {...rest} />
-            );
-        }
-    },
+    //     render() {
+    //         let { modal, className, ...rest } = this.props;
+    //         return modal ? (
+    //             <div ref={this.ref} className={merge`${className} modal`}>
+    //                 <Component {...rest}/>
+    //             </div>
+    //         ) : (
+    //             <Component className={merge`${className}`} {...rest} />
+    //         );
+    //     }
+    // },
 
     // Show wraps the given component and enables a `show` property; when `show !== true` the
     // component does not render.  Consider this a shortcut to the `<Show>` component.
